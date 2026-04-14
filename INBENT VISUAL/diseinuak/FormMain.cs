78814d0810d2fa58c20f948fbe73215b4f38abbf
hostolaza25 <hostolaza25@izarraitz.eus>
2026-04-14 09:03:14 +0200

    a jarri dut

--- INBENT VISUAL/diseinuak/FormMain.cs
+++ INBENT VISUAL/diseinuak/FormMain.cs
@@ -14,7 +14,7 @@ namespace INBENT_VISUAL.diseinuak
     {
         public FormMain()
         {
-            InitializeComponent();
+            InitializeComponent();a
         }
 
         private void InitializeComponent()
